534d8cf1f7c08a6b1bce2ece1f09bfc654aada5f
dedfynx <sio.raynaud@gmail.com>
2020-12-17 15:32:55 +0100

    Ajout Script Plateforme

---
+++ Assets/plateformeDestruBehav.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plateformeDestruBehav : MonoBehaviour
+{
+    public GameObject plateforme;
+    public int duree;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(duree <= 0){
+            Destroy(plateforme);
+        }
+        else{
+            duree--;
+        }
+    }
+}
---
+++ Assets/plateformeMouvanteBehav.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plateformeMouvanteBehav : MonoBehaviour
+{
+    public int nbFrames;
+    int y;
+    bool aller;
+    public Vector3 direction;
+    public float vit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        y =0;
+        aller = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(nbFrames > y){
+            y++;
+            if(aller){
+                transform.Translate(direction.x * vit,direction.y * vit,direction.z * vit);
+            }
+            else{
+                transform.Translate(-direction.x * vit,-direction.y * vit,-direction.z * vit);
+            }
+            
+        }
+        else{
+            y = 0;
+        }
+    }
+}
